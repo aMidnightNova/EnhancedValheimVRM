4280bd55f3df2cf47832bdcf20e70200b4e017bc
Midnight Nova <amidnightnova@gmail.com>
2026-09-16 18:37:07 -0700

    fix version error, this is 1.2 not 1.1.2

--- EnhancedValheimVRM/Plugin.cs
+++ EnhancedValheimVRM/Plugin.cs
@@ -14,7 +14,7 @@ namespace EnhancedValheimVRM
     {
         private const string PluginGuid = "com.rawrtastic.plugins.enhancedvalheimvrm";
         private const string PluginName = "EnhancedValheimVRM";
-        private const string PluginVersion = "1.1.2";
+        private const string PluginVersion = "1.2.0";
 
         private static EnhancedValheimVrmPlugin _instance;
         private static bool _clientInitialized;
